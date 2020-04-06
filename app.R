fa5fcf282c92525a342dbbb16185646de50cd8f1
dswdejonge <dsw.dejonge@gmail.com>
2020-04-06 15:02:20 +0200

    Colour markers based on value, include legend.

--- app.R
+++ app.R
@@ -114,16 +114,19 @@ server <- function(input, output) {
       map_type = input$map_type,
       database = database)
     # Palette
-    pal <- colorNumeric(
-      palette = "Blues",
+    my_pal <- colorNumeric(
+      palette = RColorBrewer::brewer.pal(11, "Spectral"), # Spectral palette
       domain = c(
-        min(my_subset$Value, na.rm = T), 
-        max(my_subset$Value, na.rm = T)))
+        min(my_subset$Value, na.rm = T), # Minimum range
+        max(my_subset$Value, na.rm = T)), # Maximum range
+      reverse = T # Use the scale in reverse (blue is low, red is high)
+      )
     # Create map
     create_map(
       my_subset = my_subset, 
       all_stations = all_stations,
-      pal = pal)
+      my_pal = my_pal,
+      map_type = input$map_type)
   })
 
   # ------------
--- map.R
+++ map.R
@@ -1,4 +1,12 @@
-create_map <- function(my_subset, all_stations, pal){
+create_map <- function(my_subset, all_stations, my_pal, map_type){
+  # Map title
+  if(map_type == "pa"){
+    map_title <- HTML("<p>Presence - Absence</p>")
+  }else if(map_type == "dens"){
+    map_title <- HTML("<p>Density (count m<sup>-2</sup>)</p>")  
+  }else if(map_type == "biom"){
+    map_title <- HTML("<p>Biomass (g AFDW m<sup>-2</sup>)</p>") 
+  }
   
   mymap <- leaflet() %>%
     #fitBounds(lng1 = lons[1], lng2 = lons[2], lat1 = lats[1], lat2 = lats[2]) %>%
@@ -7,9 +15,9 @@ create_map <- function(my_subset, all_stations, pal){
     addCircleMarkers(
       lng=my_subset$Lon_DD, 
       lat=my_subset$Lat_DD,
-      color = "red",
-      #color = ~pal(value),
-      radius = 20,
+      #color = "red",
+      color = my_pal(my_subset$Value),
+      radius = 15,
       fillOpacity = 0.5,
       stroke = FALSE
     ) %>%
@@ -20,7 +28,12 @@ create_map <- function(my_subset, all_stations, pal){
       fillOpacity = 1.0,
       color = "black",
       stroke = FALSE
-    )
+    ) %>%
+    addLegend("bottomright",
+              pal = my_pal,
+              values = my_subset$Value,
+              title = map_title,
+              opacity = 1)
   
   return(mymap)
 }
