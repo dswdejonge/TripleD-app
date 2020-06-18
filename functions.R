de8dd70894d738bfa094b0d8f48f7f05507d8dd0
dswdejonge <dsw.dejonge@gmail.com>
2020-06-18 11:34:21 +0200

    Fix bug that breaks app with no biomass for taxon.

--- functions.R
+++ functions.R
@@ -1,9 +1,16 @@
 get_palette <- function(df){
+  if(all(is.na(df$Value))){
+    min_range <- 0
+    max_range <- 1
+  }else{
+    min_range <- min(df$Value, na.rm = T)
+    max_range <- max(df$Value, na.rm = T)
+  }
   my_pal <- colorNumeric(
     palette = RColorBrewer::brewer.pal(11, "Spectral"), # Spectral palette
     domain = c(
-      min(df$Value, na.rm = T), # Minimum range
-      max(df$Value, na.rm = T)), # Maximum range
+      min_range, # Minimum range
+      max_range), # Maximum range
     reverse = T # Use the scale in reverse (blue is low, red is high)
   )
   return(my_pal)
